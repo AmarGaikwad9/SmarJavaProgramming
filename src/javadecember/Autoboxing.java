e002c6d1ea557da385c5f8b36a8f0772942eac30
AmarGaikwad9 <amar.gaykwad@gmail.com>
2025-12-13 12:50:06 +0530

    3rd commit

---
+++ src/javadecember/Autoboxing.java
@@ -0,0 +1,15 @@
+package javadecember;
+
+public class Autoboxing {
+
+    public  static void main(String[] args) {
+        int a=10;
+        Integer i = new Integer(a);
+
+
+        System.out.println("Value of i: " + i);
+        System.out.println("Value of a: " + a);
+    }
+
+
+}
